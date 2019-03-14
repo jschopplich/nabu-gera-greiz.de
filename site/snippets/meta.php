165a30da386702e7878d002fb465a8da5a43c75b
jschopplich <27850750+jschopplich@users.noreply.github.com>
2019-03-14 16:08:23 +0100

    Update meta.php

--- site/snippets/meta.php
+++ site/snippets/meta.php
@@ -7,32 +7,28 @@ $metaDescription = $page->metaDescription()->or($site->homePage()->metaDescripti
 
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2">
-<meta name="robots" content="index,follow">
 <link rel="canonical" href="<?= $page->url() ?>">
-<meta name="google-site-verification" content="pQ22isBLBAzQz-1VOB56Mk00YEIz_Lv4xKs3HAfrCig">
 
 <title><?= $metaTitle ?></title>
 
 <meta name="description" content="<?= $metaDescription ?>">
-<meta name="publisher" content="<?= $site->metaAuthor() ?>">
-<meta name="copyright" content="<?= $site->metaAuthor() ?>">
+<meta name="robots" content="index,follow">
+<meta name="google-site-verification" content="pQ22isBLBAzQz-1VOB56Mk00YEIz_Lv4xKs3HAfrCig">
 
-<meta property="og:title" content="<?= $metaTitle ?>">
-<meta property="og:description" content="<?= $metaDescription ?>">
 <meta property="og:url" content="<?= $page->url() ?>">
 <meta property="og:type" content="website">
+<meta property="og:title" content="<?= $metaTitle ?>">
+<meta property="og:description" content="<?= $metaDescription ?>">
 <meta property="og:image" content="<?= url('meta-image.jpg') ?>">
-<meta property="og:site_name" content="<?= $site->title() ?>">
-<meta property="article:author" content="<?= $site->metaAuthor() ?>">
 
 <meta name="twitter:card" content="summary">
 <?php /*
 <meta name="twitter:site" content="@site_account">
 <meta name="twitter:creator" content="@individual_account">
 */ ?>
+<meta name="twitter:url" content="<?= $page->url() ?>">
 <meta name="twitter:title" content="<?= $metaTitle ?>">
 <meta name="twitter:description" content="<?= $metaDescription ?>">
-<meta name="twitter:url" content="<?= $page->url() ?>">
 <meta name="twitter:image" content="<?= url('meta-image.jpg') ?>">
 
 <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
