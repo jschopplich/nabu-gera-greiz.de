5e4026f65523af9bd5e024f9c49450064eb8d0cc
jschopplich <27850750+jschopplich@users.noreply.github.com>
2019-03-18 10:35:20 +0100

    Add styles for old blog format

--- site/snippets/navigation/projects.php
+++ site/snippets/navigation/projects.php
@@ -2,8 +2,8 @@
 
   <a class="navbar-item<?php e($page->id() === 'naturschutzinformation-waldhaus', ' is-active') ?>" href="<?= page('projekte/naturschutzinformation-waldhaus')->url() ?>">
     <span>
-      <span class="icon has-text-grey-light">
-        <i class="fas fa-home-heart" aria-hidden="true"></i>
+      <span class="icon has-text-grey-primary">
+        <i class="fal fa-home-heart" aria-hidden="true"></i>
       </span>
       <strong><?= page('projekte/naturschutzinformation-waldhaus')->title()->html() ?></strong>
       <br>
@@ -15,7 +15,7 @@
 
   <a class="navbar-item<?php e($page->id() === 'friessnitzer-see', ' is-active') ?>" href="<?= page('projekte/friessnitzer-see')->url() ?>">
     <span>
-      <span class="icon has-text-grey-light">
+      <span class="icon has-text-grey-primary">
         <i class="fal fa-trees" aria-hidden="true"></i>
       </span>
       <strong><?= page('projekte/friessnitzer-see')->title()->html() ?></strong>
@@ -28,7 +28,7 @@
 
   <a class="navbar-item<?php e($page->id() === 'amphibienzaun', ' is-active') ?>" href="<?= page('projekte/amphibienzaun')->url() ?>">
     <span>
-      <span class="icon has-text-grey-light">
+      <span class="icon has-text-grey-primary">
         <i class="fal fa-frog" aria-hidden="true"></i>
       </span>
       <strong><?= page('projekte/amphibienzaun')->title()->html() ?></strong>
@@ -41,7 +41,7 @@
 
   <a class="navbar-item<?php e($page->id() === 'lebensraum-kirchturm', ' is-active') ?>" href="<?= page('projekte/lebensraum-kirchturm')->url() ?>">
     <span>
-      <span class="icon has-text-grey-light">
+      <span class="icon has-text-grey-primary">
         <i class="fal fa-church" aria-hidden="true"></i>
       </span>
       <strong><?= page('projekte/lebensraum-kirchturm')->title()->html() ?></strong>
@@ -54,7 +54,7 @@
 
   <a class="navbar-item<?php e($page->id() === 'honig-und-bienen', ' is-active') ?>" href="<?= page('projekte/honig-und-bienen')->url() ?>">
     <span>
-      <span class="icon has-text-grey-light">
+      <span class="icon has-text-grey-primary">
         <i class="fal fa-flower" aria-hidden="true"></i>
       </span>
       <strong><?= page('projekte/honig-und-bienen')->title()->html() ?></strong>
---
+++ site/templates/blog-old.php
@@ -0,0 +1,17 @@
+<?php snippet('header') ?>
+
+  <div class="container">
+    <div class="columns is-marginless has-pb-xlarge">
+      <div class="column section has-pt-xlarge has-pt-xxlarge-tablet has-pr-xlarge-tablet">
+        <div class="content is-main is-old-blog">
+
+          <?php snippet('intro') ?>
+          <?= $page->text()->kirbytext() ?>
+
+        </div>
+      </div>
+      <?php snippet('side') ?>
+    </div>
+  </div>
+
+<?php snippet('footer') ?>
