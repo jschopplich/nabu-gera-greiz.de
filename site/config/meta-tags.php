80d065074b9f5d61ed17e3169a70ebb08ef25722
jschopplich <27850750+jschopplich@users.noreply.github.com>
2019-04-14 17:11:03 +0200

    Simplify config setup

---
+++ site/config/meta-tags.php
@@ -0,0 +1,73 @@
+<?php
+
+return function ($page, $site) {
+    if ($page->isHomePage()) {
+        $metaTitle       = $site->homePage()->metaTitle()->value();
+        $metaDescription = $site->homePage()->metaDescription()->value();
+    } else {
+        $metaTitle       = r($page->metaTitle()->isNotEmpty(), $page->metaTitle()->value(), $page->title()->value() . ' — ' . $site->title()->value());
+        $metaDescription = r($page->metaDescription()->isNotEmpty(), $page->metaDescription()->value(), $site->homePage()->metaDescription()->value());
+    }
+    $metaImage = $page->metaImage()->isNotEmpty() && $page->metaImage()->toFile() ? $page->metaImage()->toFile()->resize(1200)->url() : url('meta-image.jpg');
+
+    return [
+        'title' => $metaTitle,
+        'meta' => [
+            'description' => $metaDescription,
+            'robots' => 'index, follow',
+            'google-site-verification' => env('GOOGLE_SITEVERIFICATION', ''),
+            'msapplication-TileColor' => env('APP_THEMECOLOR', '#ffffff'),
+            'theme-color' => env('APP_THEMECOLOR', '#ffffff')
+        ],
+        'link' => [
+            'canonical' => $page->url(),
+            'apple-touch-icon' => ['href' => '/apple-touch-icon.png', 'sizes' => '180x180'],
+            'manifest' => '/site.webmanifest',
+            'mask-icon' => ['href' => '/safari-pinned-tab.svg', 'color' => env('APP_MASKCOLOR', '#ffffff')],
+            'icon' => [
+                ['href' => '/favicon-32x32.png', 'sizes' => '32x32', 'type' =>'image/png'],
+                ['href' => '/favicon-16x16.png', 'sizes' => '16x16', 'type' =>'image/png']
+            ]
+        ],
+        'og' => [
+            'type' => 'website',
+            'url' => $page->url(),
+            'site_name' => $site->title()->value(),
+            'title' => $metaTitle,
+            'description' => $metaDescription,
+            'image' => $metaImage
+        ],
+        'twitter' => [
+            'card' => 'summary',
+            'url' => $page->url(),
+            //'site' => $site->twitter(),
+            'title' => $metaTitle,
+            'description' => $metaDescription,
+            'image' => $metaImage
+        ],
+        'json-ld' => [
+            'WebSite' => [
+                'url' => $page->url(),
+                'name' => $site->title()->value(),
+                'description' => $metaDescription,
+                'author' => [
+                    '@type' => 'Person',
+                    'name' => $site->author()->value()
+                ]
+            ],
+            'Organization' => [
+                'name' => $site->title()->value(),
+                'legalName' => $site->author()->value(),
+                'url' => $site->url(),
+                'logo' => url('assets/images/logo.svg'),
+                'foundingDate' => '2004',
+                'contactPoint' => [
+                    '@type' => 'ContactPoint',
+                    'contactType' => 'office',
+                    'email' => 'vorstand@nabu-gera-greiz.de',
+                    'url' => $site->url()
+                ]
+            ]
+        ]
+    ];
+};
