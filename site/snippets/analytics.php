b44bff4be7484e5568e28891d2a334addcf1ab90
jschopplich <27850750+jschopplich@users.noreply.github.com>
2019-05-29 10:46:52 +0200

    Update analytics.php

--- site/snippets/analytics.php
+++ site/snippets/analytics.php
@@ -1,7 +1,7 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-134185590-1"></script>
 <script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-  gtag('config', 'UA-134185590-1');
+  window.dataLayer = window.dataLayer || []
+  function gtag () { dataLayer.push(arguments) }
+  gtag('js', new Date())
+  gtag('config', 'UA-134185590-1')
 </script>
