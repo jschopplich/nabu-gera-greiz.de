2ff553bbdbb4fa8e37c18a40ad449ebdbc80e160
jschopplich <27850750+jschopplich@users.noreply.github.com>
2019-06-14 20:33:04 +0200

    Update dependencies

--- sw.js
+++ sw.js
@@ -16,7 +16,7 @@ if (workbox) {
   },
   {
     "url": "assets/fonts/SourceSerifPro/family.css",
-    "revision": "d3279d0d6d5977f31e40acfc49d1d60e"
+    "revision": "d1fcd45269400df65b645c2cd8e99844"
   },
   {
     "url": "assets/fonts/SourceSerifPro/SourceSerifPro-Bold.ttf.woff2",
