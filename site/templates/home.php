665cf4991df2961bcf7e47a569932445a817d411
Johann Schopplich <mail@johannschopplich.com>
2020-11-13 09:25:59 +0100

    Further minor layout simplifications

--- site/templates/home.php
+++ site/templates/home.php
@@ -9,23 +9,23 @@
   </div>
 </div>
 
-<div class="section is-article-cards has-background-white-bis">
+<div class="section is-article-list has-background-white-bis">
   <div class="container">
-    <h2 id="aktuelles" class="title is-3 has-text-weight-bold has-text-centered mb-5">
+    <h2 id="aktuelles" class="title is-3 has-text-weight-bold has-text-centered mb-6">
       Aktuelle Beiträge
     </h2>
 
     <div class="is-hidden-tablet">
       <?php foreach ($articles as $article): ?>
         <div class="media box" data-animere="slideInUp">
-          <div class="media-left">
+          <div class="media-left mr-5">
             <?php if ($image = $article->images()->first()): ?>
               <figure class="image is-96x96">
-                <img class="has-border-radius" src="<?= $image->crop(96, 96, 'center')->url() ?>" alt="">
+                <img src="<?= $image->crop(96, 96, 'center')->url() ?>" alt="">
               </figure>
             <?php else: ?>
-              <span class="icon is-large has-text-white-ter">
-                <span class="fas fa-2x fa-newspaper" aria-hidden="true"></span>
+              <span class="icon is-large has-text-grey-lighter">
+                <span class="fas fa-3x fa-newspaper" aria-hidden="true"></span>
               </span>
             <?php endif ?>
           </div>
@@ -57,8 +57,8 @@
                 </figure>
               <?php else: ?>
                 <figure class="image is-3by2">
-                  <span class="has-ratio card-icon icon has-text-white-ter">
-                    <svg aria-hidden="true" data-prefix="fal" data-icon="newspaper" class="svg-inline--fa fa-newspaper fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M552 64H88c-13.234 0-24 10.767-24 24v8H24c-13.255 0-24 10.745-24 24v280c0 26.51 21.49 48 48 48h504c13.233 0 24-10.767 24-24V88c0-13.233-10.767-24-24-24zM32 400V128h32v272c0 8.822-7.178 16-16 16s-16-7.178-16-16zm512 16H93.258A47.897 47.897 0 0 0 96 400V96h448v320zm-404-96h168c6.627 0 12-5.373 12-12V140c0-6.627-5.373-12-12-12H140c-6.627 0-12 5.373-12 12v168c0 6.627 5.373 12 12 12zm20-160h128v128H160V160zm-32 212v-8c0-6.627 5.373-12 12-12h168c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12H140c-6.627 0-12-5.373-12-12zm224 0v-8c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0-64v-8c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0-128v-8c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12zm0 64v-8c0-6.627 5.373-12 12-12h136c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12H364c-6.627 0-12-5.373-12-12z"></path></svg>
+                  <span class="has-ratio icon has-text-grey-lighter">
+                    <span class="fas fa-5x fa-newspaper" aria-hidden="true"></span>
                   </span>
                 </figure>
               <?php endif ?>
