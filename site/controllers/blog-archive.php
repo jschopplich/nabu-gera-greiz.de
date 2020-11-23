498bac50415d31f373b989545e929c1b85de0273
Johann Schopplich <mail@johannschopplich.com>
2020-11-23 13:32:29 +0100

    Use fixed pagination for archive blogs

--- site/controllers/blog-archive.php
+++ site/controllers/blog-archive.php
@@ -3,15 +3,14 @@
 use Kirby\Toolkit\Str;
 
 return function ($page) {
-    $parent   = $page->parent();
-    $perpage  = $parent->perPage()->int();
-    $articles = $parent
+    $articles = $page
+        ->parent()
         ->children()
         ->listed()
         ->filterBy('template', 'article')
         ->filter(fn($child) => Str::startsWith($child->date(), $page->virtualYear()))
         ->sortBy(fn($child) => $child->date()->toDate(), 'desc')
-        ->paginate($perpage);
+        ->paginate(12);
 
     return compact('articles');
 };
