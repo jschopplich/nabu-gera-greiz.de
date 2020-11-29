9739a5091e8113f989f75af507a1917c597b4b42
Johann Schopplich <mail@johannschopplich.com>
2020-11-29 20:31:22 +0100

    Fix children of secured pages

--- site/plugins/kirby-secured-pages/controllers/secured-pages-login.php
+++ site/plugins/kirby-secured-pages/controllers/secured-pages-login.php
@@ -1,27 +1,27 @@
 <?php
 
+use KirbyExtended\SecuredPages\SecuredPages;
+
 return function ($kirby) {
-    $error = false;
-    $password = get('password');
-
-    if ($kirby->request()->is('POST') && $password) {
-        $csrfToken = get('csrf');
-        if (!csrf($csrfToken)) {
-            return ['error' => 'Der CSRF-Token ist nicht korrket'];
-        }
-
-        $protectedPage = SecuredPages::findProtectedPage(get('redirect'));
-        if (!$protectedPage) {
-            return ['error' => 'Die geschütze Seite existiert nicht'];
-        }
-
-        if ($protectedPage->securedPagePassword()->value() !== $password) {
-            return ['error' => 'Das Passwort ist nicht korrekt'];
-        }
-
-        kirby()->session()->set("securedPages.access.{$protectedPage->id()}", true);
-        go(get('redirect'));
+    if (!$kirby->request()->is('POST')) {
+        return ['error' => false];
     }
 
-    return compact('error');
+    $csrfToken = get('csrf');
+    if (!csrf($csrfToken)) {
+        return ['error' => 'Der CSRF-Token ist nicht korrket'];
+    }
+
+    $redirectPage = page(get('redirect'));
+    $protectedPage = SecuredPages::findProtectedPage($redirectPage);
+    if (!$protectedPage) {
+        return ['error' => 'Die geschütze Seite existiert nicht'];
+    }
+
+    if ($protectedPage->securedPagePassword()->value() !== get('password')) {
+        return ['error' => 'Das Passwort ist nicht korrekt'];
+    }
+
+    kirby()->session()->set("securedPages.access.{$protectedPage->id()}", true);
+    go(get('redirect'));
 };
